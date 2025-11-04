d13933131af0670e2ebfcc87d52a7f604fd9e349
cmendoza26 <cmendoza26@horizon.csueastbay.edu>
2025-11-04 15:38:54 -0800

    implemented user class, customer and employee class

--- BankingSystem/src/Employee.java
+++ BankingSystem/src/Employee.java
@@ -1,4 +1,25 @@
 
 public class Employee extends User {
+	private String employeeName;
+	private static int employeeID = 0;
+	private int id;
 
+	public Employee(String username, String password, boolean loggedIn, String employeeName) {
+		super(username, password);
+		employeeID++;
+		this.employeeName = employeeName;
+		this.id = employeeID;
+	}
+	
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+	public String getEmployeeName() {
+		return this.employeeName;
+	}
+	
+	public int getEmployeeID() {
+		return this.id;
+	}
 }
