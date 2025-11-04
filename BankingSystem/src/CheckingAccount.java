ee52133b9756a66ad68c6edf3db2b4b21ae79956
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-04 15:06:17 -0800

    checkingaccount constructor

--- BankingSystem/src/CheckingAccount.java
+++ BankingSystem/src/CheckingAccount.java
@@ -1,5 +1,14 @@
-
-public class CheckingAccount {
+import java.util.ArrayList;
 
 public class CheckingAccount extends BankAccount {
+	public CheckingAccount(ArrayList<Customer> owner) {
+		id = ++count;
+		status = true;
+		type = AccountType.Checking;
+		owners = owner;
+		balance = 0;
+		transactions = new ArrayList<Transaction>();
+	}
+	
+	
 }
