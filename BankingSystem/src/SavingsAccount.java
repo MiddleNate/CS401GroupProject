72a37c6131d5f21f95302b21e75a418120b1929b
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-04 15:28:25 -0800

    attributes and constructor

--- BankingSystem/src/SavingsAccount.java
+++ BankingSystem/src/SavingsAccount.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
-public class SavingsAccount {
-
+public class SavingsAccount extends BankAccount {
+	private double interestRate;
+	private double withdrawlLimit;
+	private LocalDate lastUpdated;
+	private double withdrawnSinceUpdated;
+	
+	public SavingsAccount(ArrayList<Customer> owner, double interest, double limit) {
+		id = ++count;
+		status = true;
+		type = AccountType.Savings;
+		owners = owner;
+		balance = 0;
+		transactions = new ArrayList<Transaction>();
+		interestRate = interest;
+		withdrawlLimit = limit;
+		lastUpdated = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+		withdrawnSinceUpdated = 0;
+	}
+	
+	
 }
