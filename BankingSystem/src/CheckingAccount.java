0b238c77460016bb3508bb4a52ad433c4ff3af28
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-04 15:14:13 -0800

    checking account implementation

--- BankingSystem/src/CheckingAccount.java
+++ BankingSystem/src/CheckingAccount.java
@@ -10,5 +10,36 @@ public class CheckingAccount extends BankAccount {
 		transactions = new ArrayList<Transaction>();
 	}
 	
+	public boolean deposit(double amt) {
+		// ensure only positive amounts are being deposited
+		if (amt > 0) {
+			// truncate any extra decimal places
+			amt = Math.floor(amt * 100) / 100;
+			balance += amt;
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
+	public boolean withdraw(double amt) {
+		// ensure only positive amounts are being withdrawn and
+		// that the balance will not go below zero
+		if (amt > 0 && balance - amt >= 0) {
+			balance -= amt;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean closeAccount() {
+		// checking accounts can only be closed if their balance is 0
+		if (status == true && balance == 0) {
+			status = false;
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
