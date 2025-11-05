461c516d9ad0c3a7132c3cf988bd112e62962b6f
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-04 16:13:02 -0800

    constructor

--- BankingSystem/src/LOCAccount.java
+++ BankingSystem/src/LOCAccount.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.ChronoUnit;
 
-public class LOCAccount {
-
+public class LOCAccount extends BankAccount {
+	private double creditLimit;
+	private double interestRate;
+	private double minimumDue;
+	private LocalDate lastUpdated;
+	private double paidSinceUpdated;
+	
+	public LOCAccount(ArrayList<Customer> owner, double limit, double interest, double minimum) {
+		id = ++count;
+		status = true;
+		type = AccountType.LineOfCredit;
+		owners = owner;
+		balance = 0;
+		transactions = new ArrayList<Transaction>();
+		creditLimit = limit;
+		interestRate = interest;
+		minimumDue = minimum;
+		lastUpdated = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+		paidSinceUpdated = 0;
+	}
 }
