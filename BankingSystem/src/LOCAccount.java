72ab85a84836cdabfc4e837c84269cfc33e8ff6f
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-04 16:30:03 -0800

    update method, runs before checking balance and withdraw/pay

--- BankingSystem/src/LOCAccount.java
+++ BankingSystem/src/LOCAccount.java
@@ -53,9 +53,39 @@ public class LOCAccount extends BankAccount {
 		return paidSinceUpdated;
 	}
 	
+	@Override public double getBalance() {
+		update();
+		return balance;
+	}
+	
+	public void update() {
+		// do not update if the account is closed
+		if (!status) return;
+		
+		LocalDate currentMonth = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+		// check if at least one month has passed since the last updated date
+		if (currentMonth.isAfter(lastUpdated)) {
+			// figure out how many months have passed (how many times we need to update)
+			int numUpdates = (int) ChronoUnit.MONTHS.between(lastUpdated, currentMonth);
+			for (int i = 0; i < numUpdates; i++) {
+				// only charge interest if the minimum has not been paid
+				// if multiple months have elapsed, this will only apply 
+				// to the first month since paidSinceUpdated gets reset to 0
+				// if the balance is 0 no interest will be charged by the equation
+				if (paidSinceUpdated < minimumDue) {
+					balance *= 1 + interestRate;
+				}
+				paidSinceUpdated = 0;
+			}
+		}
+		// if no updates are needed, do nothing
+	}
+	
 	public boolean withdraw(double amt) {
 		// do not allow withdrawal if the account is closed
 		if (!status) return false;
+		
+		update();
 		
 		// since the balance represents debt owed, withdraw will add
 		// to the balance instead of subtracting
@@ -75,6 +105,8 @@ public class LOCAccount extends BankAccount {
 	public boolean pay(double amt) {
 		// do not allow payment if the amount is closed
 		if (!status) return false;
+		
+		update();
 		
 		// since the balance represents debt owed, paying with subtract
 		// from the balance instead of adding
