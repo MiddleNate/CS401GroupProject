89d6c7fe5c7a9c68f29741c20ea8a12939a8f85b
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-13 14:49:08 -0800

    transaction uses localDate instead of Date

--- BankingSystem/src/Transaction.java
+++ BankingSystem/src/Transaction.java
@@ -2,37 +2,16 @@ import java.util.*;
 import java.time.LocalDate;
 
 public class Transaction {
-<<<<<<< Updated upstream
-	protected int id;
-	protected Double amount;
-	protected Date date;
-	protected TransactionType type; 
-	protected User user;
-=======
 	private int id;
 	private Double amount;
 	private LocalDate date;
 	private TransactionType type; 
 	private User user;
->>>>>>> Stashed changes
 	
-	//Default constructor
-	public Transaction() {
-		this.amount = 0.0;
-		this.date = null;
-		this.type = null;
-		this.user = null;
-	}
-<<<<<<< Updated upstream
-	// Constructor
-	public Transaction(Double amount, TransactionType type, Date date, User user ) {
-=======
-	
-	public Transaction(Double amount, TransactionType type, LocalDate date, User user ) {
->>>>>>> Stashed changes
+	public Transaction(Double amount, TransactionType type, User user ) {
 		this.amount = amount;
-		this.date = date;
+		this.date = LocalDate.now();
 		this.type = type;
-		this.user = user;	
+		this.user = user;
 	}
 }
