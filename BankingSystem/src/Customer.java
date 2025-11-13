e2d091e1dff3a47cbba8618c05f06a3154f15b12
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-13 13:54:53 -0800

    added methods to get and set static customercount

--- BankingSystem/src/Customer.java
+++ BankingSystem/src/Customer.java
@@ -2,18 +2,25 @@ import java.util.ArrayList;
 
 public class Customer extends User {
 	private String customerName;
-	private static int customerID = 0;
+	private static int customerCount = 0;
 	private int id;
 	private int socialSecNumber;
 	private ArrayList<BankAccount> accounts;
 
 	public Customer(String username, String password, String customerName, int socialSecNumber) {
 		super(username, password);
-		customerID++;
 		this.customerName = customerName;
-		this.id = customerID;
+		this.id = ++customerCount;
 		this.socialSecNumber = socialSecNumber;
 		this.accounts = new ArrayList<BankAccount>();
+	}
+	
+	public static void setCustomerCount(int newCount) {
+		customerCount = newCount;
+	}
+	
+	public static int getCustomerCount() {
+		return customerCount;
 	}
 
 	public void setCustomerName(String customerName) {
