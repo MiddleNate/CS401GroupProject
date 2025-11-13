f11de9b193141ced0c49298fae0cfa09668813bb
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-13 12:48:54 -0800

    loading users and accounts from a file

--- BankingSystem/src/Server.java
+++ BankingSystem/src/Server.java
@@ -1,8 +1,27 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.lang.Integer;
 
 public class Server {
 
+	protected static HashMap<Integer, BankAccount> accounts;
+	protected static HashMap<String, User> users;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// loading users and accounts into memory
+		try {
+			FileInputStream userFile = new FileInputStream("users.txt");
+			ObjectInputStream userStream = new ObjectInputStream(userFile);
+			users = (HashMap<String, User>) userStream.readObject();
+			
+			FileInputStream accountFile = new FileInputStream("accounts.txt");
+			ObjectInputStream accountStream = new ObjectInputStream(accountFile);
+			accounts = (HashMap<Integer, BankAccount>) accountStream.readObject();
+		} catch (Exception e) {
+			System.out.println("File loading error: " + e + "\nExiting...");
+			System.exit(1);
+		}
 
 	}
 
