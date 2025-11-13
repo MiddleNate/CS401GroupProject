d630c17ad55683d683a5c9deeb2d30ae0804c761
Mon-Rico <ricomonica1108@gmail.com>
2025-11-13 13:59:04 -0800

    Update Transaction.java
    Using localDate instead of Date for accurate tracking for transaction history

--- BankingSystem/src/Transaction.java
+++ BankingSystem/src/Transaction.java
@@ -1,11 +1,20 @@
 import java.util.*;
+import java.time.LocalDate;
 
 public class Transaction {
+<<<<<<< Updated upstream
 	protected int id;
 	protected Double amount;
 	protected Date date;
 	protected TransactionType type; 
 	protected User user;
+=======
+	private int id;
+	private Double amount;
+	private LocalDate date;
+	private TransactionType type; 
+	private User user;
+>>>>>>> Stashed changes
 	
 	//Default constructor
 	public Transaction() {
@@ -14,8 +23,13 @@ public class Transaction {
 		this.type = null;
 		this.user = null;
 	}
+<<<<<<< Updated upstream
 	// Constructor
 	public Transaction(Double amount, TransactionType type, Date date, User user ) {
+=======
+	
+	public Transaction(Double amount, TransactionType type, LocalDate date, User user ) {
+>>>>>>> Stashed changes
 		this.amount = amount;
 		this.date = date;
 		this.type = type;
