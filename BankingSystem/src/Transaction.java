bbf6ef133c77acbf466818c96955b3e4b325eace
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-13 14:23:36 -0800

    adjusted transaction and added a static count
    # Conflicts:
    #	BankingSystem/src/Transaction.java

--- BankingSystem/src/Transaction.java
+++ BankingSystem/src/Transaction.java
@@ -3,15 +3,25 @@ import java.time.LocalDate;
 
 public class Transaction {
 	private int id;
+	private static int count;
 	private Double amount;
 	private LocalDate date;
 	private TransactionType type; 
 	private User user;
-	
-	public Transaction(Double amount, TransactionType type, User user ) {
+	private BankAccount account;
+
+	public static void setTransactionCount(int c) {
+		count = c;
+	}
+	public static int getTransactionCount() {
+		return count;
+	}
+	public Transaction(double amount, TransactionType type, User user, BankAccount account) {
+		this.id = ++count;
 		this.amount = amount;
 		this.date = LocalDate.now();
 		this.type = type;
 		this.user = user;
+		this.account = account;
 	}
 }
