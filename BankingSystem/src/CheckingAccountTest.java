32527a90ca95a88365fd3419514f1e6154937604
cmendoza26 <cmendoza26@horizon.csueastbay.edu>
2025-11-13 18:33:53 -0800

    checking and savings account tests
    deposit/withdraw and interest

--- BankingSystem/src/CheckingAccountTest.java
+++ BankingSystem/src/CheckingAccountTest.java
@@ -1,12 +1,36 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class CheckingAccountTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		testDepositAndWithdraw();
 	}
 
+	public void testDepositAndWithdraw() {
+		// customer has a checking account
+		ArrayList<Customer> customers = new ArrayList<Customer>();
+		Customer customer  = new Customer("User1", "Password", "Name", 999);
+		customers.add(customer);
+		CheckingAccount checkingAccount = new CheckingAccount(customers);
+		
+		// balance should start at 0
+		assertTrue(checkingAccount.getBalance() == 0.0);
+		
+		// depositing 100.50 should make the balance 100.50
+		checkingAccount.deposit(100.50);
+		assertTrue(checkingAccount.getBalance() == 100.50);
+		
+		// withdrawing 50.50 should make the balance 50
+		checkingAccount.withdraw(50.50);
+		assertTrue(checkingAccount.getBalance() == 50);
+		
+		// withdrawing more than what the account balance is should not change the balance
+		checkingAccount.withdraw(51);
+		assertTrue(checkingAccount.getBalance() == 50);
+	}
 }
--- BankingSystem/src/SavingsAccountTest.java
+++ BankingSystem/src/SavingsAccountTest.java
@@ -10,14 +10,52 @@ public class SavingsAccountTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		testDepositWithdraw();
+		testInterest();
 	}
 	
-	public void testInterest() {
+	public void testDepositWithdraw() {
+		ArrayList<Customer> customers = new ArrayList<Customer>();
+		Customer customer  = new Customer("User1", "Password", "Name", 999);
+		customers.add(customer);
 		
-		Clock fixedClock = Clock.fixed(Instant.parse("2020-01-01T14:00:00.00Z"), ZoneId.of("UTC"));
-		SavingsAccount.setClock(fixedClock);
+		// savings account starts with 5% monthly interest and 500 withdrawal limit
+		SavingsAccount savingsAccount = new SavingsAccount(customers, 0.05, 500);
+		
+		// balance should start at 0
+		assertTrue(savingsAccount.getBalance() == 0.0);
+		
+		// depositing 1000 should make the balance 1000
+		savingsAccount.deposit(1000);
+		assertTrue(savingsAccount.getBalance() == 1000);
+		
+		savingsAccount.withdraw(1001);
+		assertTrue(savingsAccount.getBalance() == 1000);
+		
+		// can't withdraw the limit
+		savingsAccount.withdraw(500);
+		assertTrue(savingsAccount.getBalance() == 1000);
+		
+		// successful withdraw
+		savingsAccount.withdraw(100);
+		assertTrue(savingsAccount.getBalance() == 900);
 		
 	}
 
+	public void testInterest() {
+		ArrayList<Customer> customers = new ArrayList<Customer>();
+		Customer customer  = new Customer("User1", "Password", "Name", 999);
+		customers.add(customer);
+		// savings account starts with 5% monthly interest and 500 withdrawal limit
+		Clock fixedClock = Clock.fixed(Instant.parse("2020-01-01T14:00:00.00Z"), ZoneId.of("UTC"));
+		SavingsAccount.setClock(fixedClock);
+		SavingsAccount savingsAccount = new SavingsAccount(customers, 0.05, 500);
+		
+		savingsAccount.deposit(1000);
+		SavingsAccount.setClock(Clock.fixed(Instant.parse("2020-02-02T14:00:00.00Z"), ZoneId.of("UTC")));
+		System.out.println(savingsAccount.getBalance());
+		System.out.println(savingsAccount.getBalance());
+		assertTrue(savingsAccount.getBalance() == 1050.0);
+		
+	}
 }
