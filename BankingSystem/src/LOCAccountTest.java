aee27f89b9b42a22ca793be5116f2a844205d615
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-13 16:06:25 -0800

    added starter clock syntax for savings and loc testers

--- BankingSystem/src/LOCAccountTest.java
+++ BankingSystem/src/LOCAccountTest.java
@@ -1,4 +1,8 @@
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 
 import org.junit.Test;
 
@@ -8,5 +12,11 @@ public class LOCAccountTest {
 	public void test() {
 		fail("Not yet implemented");
 	}
-
+	
+	public void testInterest() {
+		
+		Clock fixedClock = Clock.fixed(Instant.parse("2020-01-01T14:00:00.00Z"), ZoneId.of("UTC"));
+		LOCAccount.setClock(fixedClock);
+		
+	}
 }
--- BankingSystem/src/SavingsAccountTest.java
+++ BankingSystem/src/SavingsAccountTest.java
@@ -1,4 +1,8 @@
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 
 import org.junit.Test;
 
@@ -7,6 +11,13 @@ public class SavingsAccountTest {
 	@Test
 	public void test() {
 		fail("Not yet implemented");
+	}
+	
+	public void testInterest() {
+		
+		Clock fixedClock = Clock.fixed(Instant.parse("2020-01-01T14:00:00.00Z"), ZoneId.of("UTC"));
+		SavingsAccount.setClock(fixedClock);
+		
 	}
 
 }
