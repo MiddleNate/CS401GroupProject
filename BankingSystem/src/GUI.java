3e7561387df4a6ad00d71201d805ab12255ed88a
Mon-Rico <ricomonica1108@gmail.com>
2025-11-13 16:17:29 -0800

    GUI Frame set up with CardLayout

--- BankingSystem/src/GUI.java
+++ BankingSystem/src/GUI.java
@@ -1,7 +1,61 @@
+import java.util.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import java.awt.*;
 
-public class GUI implements Runnable {
+public class GUI implements Runnable{
+	private CardLayout cardLayout;
+	private JPanel mainPanel;
+	private JFrame frame;
 	
 	public void run() {
+      frame.add(mainPanel);
+      frame.setVisible(true);
+        
+	}
+	
+	public GUI() {
+		
+		// --- Set up frame ---
+        frame.setTitle("Switch Panel Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null);
+        
+        // --- Create card layout ---
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+        
+        doLoginScreen();
+        
+    }
+	public void doLoginScreen() {
+
+	}
+	public void doOpenOrCloseAccount() {
 		
 	}
+	public void doBankAccounts() {
+		
+	}
+	public void doBankAccountDetails() {
+		
+	}
+	public void doTransactionMessage() {
+
+	}
+	public void doSuccessMessage() {
+
+	}
+	public void doFailMessage() {
+	}
+	public void doInvalidMessage() {
+	}
+	public void doAccountUpdatedMessage() {
+
+	}
+	
 }
