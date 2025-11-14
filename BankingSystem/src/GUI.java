8bbcaa838c31540544e0b43e2e700e231f10c052
Mon-Rico <ricomonica1108@gmail.com>
2025-11-13 16:41:15 -0800

    created loginPanel, clientPanel & employeePanel, all incomplete; Saving user input as 2 string objects

--- BankingSystem/src/GUI.java
+++ BankingSystem/src/GUI.java
@@ -4,7 +4,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI implements Runnable{
 	private CardLayout cardLayout;
@@ -33,6 +37,60 @@ public class GUI implements Runnable{
         
     }
 	public void doLoginScreen() {
+		// --- Login Attributes ---
+		String savedUserName;
+		String savedPassword;
+		// --- Set up frame ---
+        frame.setTitle("Switch Panel Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null);
+        
+        // --- Create card layout ---
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+        
+		// --- Panel 1: Login panel with textfields ---
+        JPanel loginPanel = new JPanel();
+        loginPanel.setLayout(new BorderLayout());
+        // --- Show 2 textFields ---
+        JTextField userNameTxtField = new JTextField(20);
+        JTextField passwordTxtField = new JTextField(20);
+        JButton submitBtn = new JButton("Enter");
+        
+        // --- Add All attributes ---
+        loginPanel.add(new JLabel("Logo Here"));
+        loginPanel.add(new JLabel("Welcome! Please Login"));
+        loginPanel.add(userNameTxtField);
+        loginPanel.add(passwordTxtField);
+        loginPanel.add(submitBtn);
+
+        // --- Add both panels to the main panel ---
+        mainPanel.add(loginPanel,"LOGIN");
+        // --- Add functionality to buttons ---
+        //saves input as 2 string objects
+        submitBtn.addActionListener(new ActionListener() {
+        		@Override
+        		public void actionPreformed(ActionEvent e) {
+        			savedUserName = userNameTxtField.getText();
+        			savedPassword = passwordTxtField.getText();
+        			
+        		}
+        });
+        
+        // --- Panel 2: Client Panel with list of available transactions ---
+        JPanel clientPanel = new JPanel();
+        clientPanel.setLayout(cardLayout);
+        
+        // --- Panel 3: Employee Panel with list of available transactions ---
+        JPanel employeePanel = new JPanel();
+        employeePanel.setLayout(cardLayout);
+        
+        // --- Add both panels to the main panel ---
+        mainPanel.add(clientPanel,"CLIENT");
+        mainPanel.add(employeePanel,"EMPLOYEE");
+
+
 
 	}
 	public void doOpenOrCloseAccount() {
