0a3e63ec685045381cab22096fc7d05c153ff107
cmogilefsky <18543463+MiddleNate@users.noreply.github.com>
2025-11-13 18:57:19 -0800

    transaction handling for employees
    same as for customers except that it can be performed on any account that exists

--- BankingSystem/src/Server.java
+++ BankingSystem/src/Server.java
@@ -221,7 +221,26 @@ public class Server {
 							
 							break; }
 						case MessageType.Transaction: {
+							Message reply = null;
 							
+							// check that the account specified in the message
+							// is an account that exists
+							if (Server.accounts.containsKey(m.getTransaction().getAccount().getID())) {
+								reply = new Message(MessageType.Fail);
+							} else {
+								try {
+									// pass the message to the account to be tried
+									Server.accounts.get(m.getTransaction().getAccount().getID()).tryTransaction(m.getTransaction());
+									// if no exception was thrown, the transaction was successful
+									// so we can set the reply to success
+									reply = new Message(MessageType.Success);
+								} catch (Exception e) {
+									reply = new Message(MessageType.Fail);
+								}
+							}
+							
+							// send the reply
+							out.writeObject(reply);
 							break; }
 						case MessageType.CreateCustomer: {
 							
