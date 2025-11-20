23ad91d2c861f0fb1bde17704996e32584437d8f
cmendoza26 <cmendoza26@horizon.csueastbay.edu>
2025-11-20 13:44:15 -0800

    test login, logout, and added exceptions to usertest

--- BankingSystem/src/UserTest.java
+++ BankingSystem/src/UserTest.java
@@ -7,19 +7,39 @@ public class UserTest {
 	@Test
 	public void test() throws Exception {
 		testConstructor();
-		testIfLoggedIn();
+		testLoginLogout();
+		testExceptions();
 	}
-
+	
 	public void testConstructor() {
-		User user = new User("User1", "password");
-		assertEquals("User1", user.getUsername());
+		User user = new User("username", "password");
+		assertEquals("username", user.getUsername());
 		assertEquals("password", user.getPassword());
 	}
 	
-	public void testIfLoggedIn() throws Exception {
-		User user = new User("User1", "password");
+	public void testLoginLogout() throws Exception {
+		User user = new User("username", "password");
+		// user should not be logged in yet
 		assertEquals(false, user.isLoggedIn());
-		user.tryLogin("User1", "password");
+		// test login
+		user.tryLogin("username", "password");
 		assertEquals(true, user.isLoggedIn());
+		// test logout
+		user.logout();
+		assertEquals(false, user.isLoggedIn());
+	}
+	
+	public void testExceptions() throws Exception {
+		User user = new User("User1", "password");
+		
+		// test if already logged in
+		user.tryLogin("username", "password");
+		Exception alreadyLoggedException = assertThrows(Exception.class, () -> user.tryLogin("username", "password"));
+		assertEquals("Already logged in", alreadyLoggedException.getMessage());
+		
+		// test for invalid user name and password
+		user.logout();
+		Exception invalidInfoException = assertThrows(Exception.class, () -> user.tryLogin("user", "pass"));
+		assertEquals("Invalid username or password", invalidInfoException.getMessage());
 	}
 }
